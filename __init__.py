4f5108d70df68dcf17aa33a0a21fbdcf2ba26e7d
Eric Tsai <chunchieh@gmail.com>
2013-03-18 19:09:50 +0800

    1. New API _send_request for http GET request. 2. Grab the GPS data when user request.

--- __init__.py
+++ __init__.py
@@ -4,4 +4,8 @@
 if __name__ == '__main__':
     import nikeplus
     activity_list = nikeplus.get_activity_list()
-    print activity_list
+    for activity in activity_list:
+        print '--------------------'
+        print 'Activity Id: %s' % activity.id
+        print 'Number of GPS data:  %d' % len(activity.way_points)
+        print '--------------------'
--- nikeplus.py
+++ nikeplus.py
@@ -4,9 +4,6 @@ import json
 
 API_URL = 'https://api.nike.com/me/sport'
 
-APPID   = 'fuelband'
-ACCEPT  = 'application/json'
-
 # TODO: OAuth
 TOKEN   = ''
 
@@ -14,6 +11,8 @@ class Activity(object):
     def __init__(self, activity_data={}):
         # prepare activity data
         self._parse_activity_data(activity_data)
+        # GPS data
+        self._way_points = None
 
     def __str__(self):
         return '%s(%s)' % (self.__class__, self.id)
@@ -25,25 +24,48 @@ class Activity(object):
         self.duration   = data.get('duration', '')
         self.start_time = data.get('startTime', '')
 
+    @property
+    def way_points(self):
+        if self._way_points is None:
+            self._way_points = self._get_gps_data()
+        return self._way_points
+
+    def _get_gps_data(self):
+        url = API_URL + '/activities/%s/gps' % self.id
+        response = _send_request(url)
+        return response.get('waypoints', [])
+
 def get_activity_list():
     activities = []
 
-    # TODO: offset, count, startDate, endDate
-    params = {'access_token': TOKEN}
-    url = API_URL + '/activities' + '?' + urllib.urlencode(params)
-
-    data = None
-    headers = {'appid': APPID,
-               'Accept': ACCEPT}
-
-    request = urllib2.Request(url, data, headers)
-    file = urllib2.urlopen(request)
+    # TODO: params (offset, count, startDate, endDate)
+    url = API_URL + '/activities'
+    response = _send_request(url)
 
     # create activity objects
     # TODO: next
-    response = json.load(file)
     for item in response['data']:
         obj = Activity(item)
         activities.append(obj)
 
     return activities
+
+def _send_request(url, params={}):
+    params['access_token'] = TOKEN
+    url += '?' + urllib.urlencode(params)
+
+    try:
+        data = None
+        headers = {
+            'appid': 'fuelband',
+            'Accept': 'application/json'}
+
+        request = urllib2.Request(url, data, headers)
+        file    = urllib2.urlopen(request)
+
+        response = json.load(file)
+    except urllib2.HTTPError as err:
+        print 'Error: _send_request(%s) = %s' % (url, err.read())
+        response = {}
+
+    return response
