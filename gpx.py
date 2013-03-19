242f639e5f7d81c8b2aaa8a5f52f5a6ddf971d7a
Eric Tsai <chunchieh@gmail.com>
2013-03-19 16:35:50 +0800

    Add element: wpt

--- gpx.py
+++ gpx.py
@@ -28,3 +28,31 @@ class GPX(Element):
         f = open(filename, 'w')
         ElementTree(self).write(f)
         f.close()
+
+class WayPoint(Element):
+    def __init__(self):
+        # Required Attributes
+        attrib = {
+            'lat': '0',
+            'lon': '0'}
+        Element.__init__(self, 'wpt', attrib)
+
+    @property
+    def lat(self):
+        return float( self.get('lat', 0) )
+
+    @lat.setter
+    def lat(self, value):
+        assert(value <= 90)
+        assert(value >= -90)
+        self.set('lat', str(value))
+
+    @property
+    def lon(self):
+        return float( self.get('lon', 0) )
+
+    @lon.setter
+    def lon(self, value):
+        assert(value <= 180)
+        assert(value >= -180)
+        self.set('lon', str(value))
